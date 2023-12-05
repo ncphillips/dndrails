f0d6d91067409641b3c03cca6841be29a6562bde
nolan <nolan@ncphi.com>
2023-12-04 21:47:45 -0400

    campaigns can be seen

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,5 +1,15 @@
 class CampaignsController < ApplicationController
+  before_action :set_campaign, only: [:show]
   def index
     @campaigns = Campaign.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_campaign
+    @campaign = Campaign.find(params[:id])
   end
 end
--- test/system/campaigns_test.rb
+++ test/system/campaigns_test.rb
@@ -4,6 +4,12 @@ class CampaignsTest < ApplicationSystemTestCase
   test "visiting the index" do
     visit campaigns_url
 
-    assert_selector "h1", text: "Campaign"
+    assert_selector "h1", text: "Campaigns"
+  end
+
+  test "viewing a campaign" do
+    visit campaign_url(campaigns(:one))
+
+    assert_selector "h1", text: campaigns(:one).title
   end
 end
