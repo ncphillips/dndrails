df774838d65e1bc7651ba9980b630c51404546dc
nolan <nolan@ncphi.com>
2023-12-05 11:46:12 -0400

    organize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,25 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Rails Router Docs - [https://guides.rubyonrails.org/routing.html]
 
+  # =========
+  # Home page
+  # =========
+  root to: "root#index"
+
+  # =========
+  # Campaigns
+  # =========
+  resources :campaigns
+
+  # ==============
+  # Authentication
+  # ==============
+  devise_for :users
+
+  # =============
+  # Health Checks
+  # =============
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  resources :campaigns
-
-  devise_for :users
-
-  root to: "root#index"
 end
