5e4878f8ddde2356d9925c5c9e17c10b6591e67d
nolan <nolan@ncphi.com>
2023-12-05 11:53:16 -0400

    anonymous users cannot access the index

--- test/system/campaign/index_campaigns_test.rb
+++ test/system/campaign/index_campaigns_test.rb
@@ -1,6 +1,12 @@
 require "application_system_test_case"
 
 class IndexCampaignsTest < ApplicationSystemTestCase
+  test "anonymous users cannot access the index" do
+    visit campaigns_url
+
+    assert_unauthenticated
+  end
+
   test "user can see their own campaigns" do
     sign_in users(:brent)
 
