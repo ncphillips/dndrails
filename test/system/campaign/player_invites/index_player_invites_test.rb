4f8c494af49cc0ab6c23d84e00d6f7da0a7431f3
nolan <nolan@ncphi.com>
2023-12-10 09:59:38 -0400

    confirm you only see invites for that campaign

--- test/system/campaign/player_invites/index_player_invites_test.rb
+++ test/system/campaign/player_invites/index_player_invites_test.rb
@@ -15,4 +15,18 @@ class IndexPlayerInvitesTest < ApplicationSystemTestCase
       assert_text player_invite.email
     end
   end
+
+  test "you don't see player invites for other campaigns" do
+    the_campaign = campaigns(:curse_of_strahd)
+    other_campaign = campaigns(:mountain_of_madness)
+    other_campaign.player_invites.create!(email: "somethign@example.com", invited_by: other_campaign.owner)
+
+    sign_in the_campaign.owner
+
+    visit campaign_player_invites_url(the_campaign)
+
+    other_campaign.player_invites.each do |player_invite|
+      refute_text player_invite.email
+    end
+  end
 end
